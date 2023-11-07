f8ceae2bfa7eb21dc3370498c29c00f55709c5ba
Movash <andrushhenko97@gmail.com>
2023-11-07 22:02:18 +0200

    finish

--- src/homework/1.tsx
+++ src/homework/1.tsx
@@ -1,14 +1,23 @@
 import React, { useEffect, useRef } from 'react';
 
-// Опишіть Props
+interface Props {
+  children: React.ReactNode;
+  onContentEndVisible: () => void;
+}
+
+type ObserverOptions = {
+  rootMargin: string;
+  threshold: number;
+  root: Element | null;
+};
+
 export function Observer({ children, onContentEndVisible }: Props) {
-  // Вкажіть правильний тип для useRef зверніть увагу, в який DOM елемент ми його передаємо
-  const endContentRef = useRef(null);
+
+  const endContentRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
-    // Вкажіть правильний тип для options, підказка, клас також можна вказувати як тип
-    const options = {
-      rootMargin: '0px',
+    const options: ObserverOptions = {
+      rootMargin: "0px",
       threshold: 1.0,
       root: null,
     };
--- src/homework/2.tsx
+++ src/homework/2.tsx
@@ -1,5 +1,18 @@
 import React, {useReducer} from "react";
 
+type State = {
+  isRequestInProgress: boolean;
+  requestStep: string;
+};
+
+type Action = {
+  type:
+    | "START_REQUEST"
+    | "PENDING_REQUEST"
+    | "FINISH_REQUEST"
+    | "RESET_REQUEST";
+};
+
 const initialState: State = {
   isRequestInProgress: false,
   requestStep: 'idle',
